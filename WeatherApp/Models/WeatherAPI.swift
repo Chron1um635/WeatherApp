cf048cb07207eca4b4110cee3266f25c099d1a0d
Chron1um635 <chron1um522@gmail.com>
2025-05-30 12:30:06 +0300

    implemented model

--- WeatherApp/Models/WeatherAPI.swift
+++ WeatherApp/Models/WeatherAPI.swift
@@ -5,4 +5,27 @@
 //  Created by Максим Назаров on 29.05.2025.
 //
 
-import Foundation
+struct Condition: Decodable {
+    let text: String
+    let icon: String
+}
+
+struct Day: Decodable {
+    let avgtemp_c: Double
+    let maxwind_kph: Double
+    let avghumidity: Double
+    let condition: Condition
+}
+
+struct ForecastDay: Decodable {
+    let date: String
+    let day: Day
+}
+
+struct Forecast: Decodable {
+    let forecastday: [ForecastDay]
+}
+
+struct WeatherAPI: Decodable {
+    let forecast: Forecast
+}
